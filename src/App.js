5fc956e3148ccf47b1d363061ea54ceb4af77402
aPurpleDev <chemseddine.henni@live.fr>
2019-11-15 11:03:17 +0100

    updating styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from "react";
+import DragItem from "./DragItem";
+import { Grid, GridImage, GridItem } from "./Grid";
+import GridContext from "./GridContext";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { items, moveItem } = useContext(GridContext);
+
+    return (
+        <div className="App">
+            <h1>Drag and Drop with React!</h1>
+            <Grid>
+                {items.map(item => (
+                    <DragItem key={item.id} id={item.id} onMoveItem={moveItem}>
+                        <GridItem>
+                            <GridImage src={item.src}></GridImage>
+                        </GridItem>
+                    </DragItem>
+                ))}
+            </Grid>
+        </div>
+    );
 }
 
 export default App;
